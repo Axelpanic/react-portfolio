32ca8f6bd928b691564804ca3eeeffc48f5a4468
AxelPanic <alexthickey@gmail.com>
2023-04-18 09:40:23 -0500

    adding blank files and folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const App = () => <PortfolioContainer />;
+//App building
+function App() {
+    return (
+         <div> 
+            <Header />
+            <PortfolioContainer />
+            <Footer />
+        </div>
+    )
+}
 
 export default App;
