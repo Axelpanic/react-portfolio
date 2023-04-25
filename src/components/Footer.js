490ef184a018c57006b36c4044a264b5258932f4
AxelPanic <alexthickey@gmail.com>
2023-04-25 17:56:18 -0500

    adding styling to porfolio.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Style from "../assets/style/footer.css";
 function Footer() {
     return (
         <div className='Footer'>
-            <p> Alex Hickey, written using MERN, April 2023</p>
+            <p> Alex Hickey, April 2023</p>
         </div>
     )
 };
--- src/components/pages/Deployed.js
+++ src/components/pages/Deployed.js
@@ -11,21 +11,21 @@ export default function Deployed() {
     <div className='cardish'>
       <div className='row'>
         <div className='col'>
-          <a href="https://github.com/Axelpanic/JATE" target='_blank' rel='noreferrer'>
-          <img src={JATE} /></a>
+          <a className="img-fluid" href="https://github.com/Axelpanic/JATE" target='_blank' rel='noreferrer'>
+          <img className="mw-25"  src={JATE} alt="just Another Text Editor project" /></a>
         </div>
       </div>
       <div className='row'>
         <div className='col'>
-          <a href="https://github.com/Axelpanic/improved-guacamole" target="_blank" rel='noreferrer'>
-            <img src={Scheduler} />
+          <a className="img-fluid" href="https://github.com/Axelpanic/improved-guacamole" target="_blank" rel='noreferrer'>
+            <img className="mw-25"  src={Scheduler} alt='Daily Scheduler project'/>
           </a>
         </div>
       </div>
       <div className='row'>
         <div className='col'>
-          <a href='https://github.com/Axelpanic/Weather-wherever' target='_blank' rel='noreferrer'>
-            <img src={Weather} />
+          <a className="img-fluid" href='https://github.com/Axelpanic/Weather-wherever' target='_blank' rel='noreferrer'>
+            <img className="mw-25"  src={Weather} alt='Weather Dashboard project' />
           </a>
         </div>
       </div>
