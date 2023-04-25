3e6c877519ef759e9f324a8778968b87ded03cc4
AxelPanic <alexthickey@gmail.com>
2023-04-25 08:04:53 -0500

    fixing deployment. Un-deploy from pc and styling changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
 import Header from './components/header';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import Style from './assets/style/style.css';
 
 
--- src/components/pages/Deployed.js
+++ src/components/pages/Deployed.js
@@ -8,24 +8,24 @@ export default function Deployed() {
 <div className='content'>
     <h3>Deployed Projects</h3>
     <subtitle>Click on a image below to view the repository!</subtitle>
-    <div className='cardish'>
+    <div className=''>
       <div className='row'>
         <div className='col'>
-          <a href="https://github.com/Axelpanic/JATE" target='_blank' rel='noreferrer'>
-          <img src={JATE} /></a>
+          <a className="img-fluid" href="https://github.com/Axelpanic/JATE" target='_blank' rel='noreferrer'>
+          <img  className="mw-25" src={JATE} /></a>
         </div>
       </div>
       <div className='row'>
         <div className='col'>
-          <a href="https://github.com/Axelpanic/improved-guacamole" target="_blank" rel='noreferrer'>
-            <img src={Scheduler} />
+          <a className="img-fluid" href="https://github.com/Axelpanic/improved-guacamole" target="_blank" rel='noreferrer'>
+            <img  className="mw-25" src={Scheduler} />
           </a>
         </div>
       </div>
       <div className='row'>
         <div className='col'>
-          <a href='https://github.com/Axelpanic/Weather-wherever' target='_blank' rel='noreferrer'>
-            <img src={Weather} />
+          <a className="img-fluid" href='https://github.com/Axelpanic/Weather-wherever' target='_blank' rel='noreferrer'>
+            <img className="mw-25" src={Weather} />
           </a>
         </div>
       </div>
