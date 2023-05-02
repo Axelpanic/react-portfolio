da26361adb32610545287b4dfd7f3c9578970ecd
Alex Hickey <112765530+Axelpanic@users.noreply.github.com>
2023-05-02 14:55:36 -0500

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,13 +6,13 @@ export default function About() {
     <div className='content'>
       <h2>About Page</h2>
       <div className='cardish'>
-        <p> I started working like most people do, I worked for my local stores! Gradually, I began to find a line of work that I liked. This happened to be security work. Beyond patrolling, monitoring cameras, and satifying the client's needs, I began to develop
-          a love for computers and modifying its code. I got into raspberry pi linux programing and modifying its uses. Some of the projects I still use today are :Pi-Hole, PlexPi, and Server Pi. These got my mom, a project manager and web designer, to
-          tell me about the University of Wisconsin's coding bootcamp.
-        </p>
-        <p>
-          At 31 years old, I went from almost 10 years of security experience, 5 years of managment experience, to jump into coding as a career instead of jumping from security job to security job. I am looking for a entry level position as a frontend development
-          or full stack developer. You can find out my skills on the deployed page or in the skills page!
+        <p> I am Alex Hickey and I will graduate from the University of Wisconsin Fullstack Development Bootcamp in May, 2023. 
+
+After ten years in security management where I led armed teams licensed by the Department of Defense and bank conglomerates, I decided to formalize my education in what I had spent my free time doing - working to program and refactor code. I initially taught myself how to program using linux and raspberry pi, leveraging pi-hole, PlexPi and ServerPi. 
+
+In 2022, I wanted to expand my knowledge. My mom has more than 30 years of experience in software product management and UX design, and she suggested I enroll in bootcamp. 
+
+I've spent the past eight months learning the fundamentals of front-end and backend development, as well as the software development lifecycle (SDLC), and the principles of Agile. I am seeking an entry-level position where I can continue to improve my skills and experience. 
         </p>
       </div>
     </div>
